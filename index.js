fe400b6a8001cf58cce06544d0fae5dfdbb048d4
Karel Hala <karelhala.91@gmail.com>
2018-03-13 20:24:23 +0100

    feat(phase-2): When tasks file not found do not read it

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 const chalk = require('chalk');
 const { markDone, newTodo } = require('./lib/inquirer');
-const { changeToDoFile, readToDos } = require('./lib/files');
+const { changeToDoFile } = require('./lib/files');
+const { printTasks } = require('./lib/common');
 
 const argv = require('minimist')(process.argv.slice(2));
-
-function printTasks() {
-  const tasksFile = readToDos();
-  if (tasksFile.length > 0) {
-    console.log(chalk.bold.greenBright('Good luck on these tasks:'));
-    console.log(chalk.bold.blue(tasksFile));
-  }
-}
 
 changeToDoFile(argv.f || argv.file);
 const isAdd = argv.a || argv.add;
@@ -18,9 +11,13 @@ const isMark = argv.m || argv.mark;
 const isList = argv.l || argv.list;
 
 if (isAdd && !isList) {
-  newTodo().then(() => isMark && markDone()).then(() => printTasks());
+  newTodo()
+    .then(() => isMark && markDone())
+    .then(() => printTasks());
 } else if (!isList) {
-  newTodo().then(() => markDone()).then(() => printTasks());
+  newTodo()
+    .then(() => markDone())
+    .then(() => printTasks());
 }
 
 if (isList) {
