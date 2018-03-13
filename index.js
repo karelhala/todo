58860ab6c002d67a6a1aa29301051c68fc84264e
Karel Hala <khala@redhat.com>
2018-03-13 18:22:26 +0100

    feat(stage-1): Todo in it's first stage
    You can add new todos, mark them as done and list all todos

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 const chalk = require('chalk');
-const clear = require('clear');
-const figlet = require('figlet');
+const { markDone, newTodo } = require('./lib/inquirer');
+const { changeToDoFile, readToDos } = require('./lib/files');
+
+const argv = require('minimist')(process.argv.slice(2));
+
+function printTasks() {
+  const tasksFile = readToDos();
+  if (tasksFile.length > 0) {
+    console.log(chalk.bold.greenBright('Good luck on these tasks:'));
+    console.log(chalk.bold.blue(tasksFile));
+  }
+}
+
+changeToDoFile(argv.f || argv.file);
+const isAdd = argv.a || argv.add;
+const isMark = argv.m || argv.mark;
+const isList = argv.l || argv.list;
+
+if (isAdd && !isList) {
+  newTodo().then(() => isMark && markDone()).then(() => printTasks());
+} else if (!isList) {
+  newTodo().then(() => markDone()).then(() => printTasks());
+}
+
+if (isList) {
+  printTasks();
+}
