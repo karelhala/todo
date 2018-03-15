7f655540f533a59337c6081358dc82a40da80793
Karel Hala <khala@redhat.com>
2018-03-15 11:20:23 +0100

    feat(Usage): Add usage and help options

--- index.js
+++ index.js
@@ -2,29 +2,31 @@
 
 const chalk = require('chalk');
 const { markDone, newTodo } = require('./lib/inquirer');
+const { version } = './package.json';
 const { changeToDoFile } = require('./lib/files');
-const { printTasks } = require('./lib/common');
+const { printUsage } = require('./lib/usage');
+const { printTasks, IS_MARK, IS_VERSION, IS_LIST, IS_HELP, IS_ADD, IS_FILE } = require('./lib/common');
 
-const argv = require('minimist')(process.argv.slice(2));
+if (IS_VERSION) {
+  console.log(version);
+} else if (IS_HELP) {
+  printUsage()
+} else {
+  changeToDoFile(IS_FILE);
+  if (IS_ADD && !IS_LIST) {
+    newTodo()
+      .then(() => IS_MARK && markDone())
+      .then(() => printTasks());
+  } else if (!IS_ADD && IS_MARK && !IS_LIST) {
+    markDone()
+      .then(() => printTasks());
+  } else if (!IS_LIST) {
+    newTodo()
+      .then(() => markDone())
+      .then(() => printTasks());
+  }
 
-changeToDoFile(argv.f || argv.file);
-const isAdd = argv.a || argv.add;
-const isMark = argv.m || argv.mark;
-const isList = argv.l || argv.list;
-
-if (isAdd && !isList) {
-  newTodo()
-    .then(() => isMark && markDone())
-    .then(() => printTasks());
-} else if (!isAdd && isMark && !isList) {
-  markDone()
-    .then(() => printTasks());
-} else if (!isList) {
-  newTodo()
-    .then(() => markDone())
-    .then(() => printTasks());
-}
-
-if (isList) {
-  printTasks();
+  if (IS_LIST) {
+    printTasks();
+  }
 }
