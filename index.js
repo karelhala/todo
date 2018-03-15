00eb73449969bacb4dcc436a59cd42eecefd2bd9
Karel Hala <khala@redhat.com>
2018-03-15 14:15:34 +0100

    feat(Refactor): Change state machine for choosing which questions should be asked

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
 
-const chalk = require('chalk');
-const { markDone, newTodo } = require('./lib/inquirer');
+const {runQuestions, createQuestion } = require('./lib/inquirer');
 const { version } = require('./package.json');
 const { changeToDoFile } = require('./lib/files');
 const { printUsage } = require('./lib/usage');
-const { printTasks, IS_MARK, IS_VERSION, IS_LIST, IS_HELP, IS_ADD, IS_FILE } = require('./lib/common');
+const { printTasks, IS_VERSION, IS_HELP, IS_FILE } = require('./lib/common');
 
 if (IS_VERSION) {
   console.log(version);
@@ -13,20 +12,5 @@ if (IS_VERSION) {
   printUsage()
 } else {
   changeToDoFile(IS_FILE);
-  if (IS_ADD && !IS_LIST) {
-    newTodo()
-      .then(() => IS_MARK && markDone())
-      .then(() => printTasks());
-  } else if (!IS_ADD && IS_MARK && !IS_LIST) {
-    markDone()
-      .then(() => printTasks());
-  } else if (!IS_LIST) {
-    newTodo()
-      .then(() => markDone())
-      .then(() => printTasks());
-  }
-
-  if (IS_LIST) {
-    printTasks();
-  }
+  runQuestions(createQuestion()).then(() => printTasks());
 }
